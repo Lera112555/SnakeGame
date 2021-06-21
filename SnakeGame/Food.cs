bf7914bf01f5ca5d965587159bc5748d4a6b3320
Lera112555 <73837980+Lera112555@users.noreply.github.com>
2021-06-21 10:18:27 +1000

    червь

--- SnakeGame/Food.cs
+++ SnakeGame/Food.cs
@@ -4,22 +4,22 @@ using System.Windows.Controls;
 
 namespace SnakeGame
 {
-    class Food
-    {
-        public double x,y;
-        public Ellipse ellFood = new Ellipse();
-        public Food(double x,double y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-        public void SetFoodPosition()
-        {
-            ellFood.Width = 8;
-            ellFood.Height = 10;
-            ellFood.Fill = Brushes.Yellow;
-            Canvas.SetLeft(ellFood, x);
-            Canvas.SetTop(ellFood,y);
-        }
-    }
+     class Food
+     {
+         public double x,y;
+          public Ellipse ellFood = new Ellipse();
+          public Food(double x,double y)
+         {
+             this.x = x;
+             this.y = y;
+         }
+         public void SetFoodPosition()
+         {
+             ellFood.Width = 8;
+              ellFood.Height = 10;
+             ellFood.Fill = Brushes.Yellow;
+             Canvas.SetLeft(ellFood, x);
+             Canvas.SetTop(ellFood,y);
+         }
+     }
 }
--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -4,23 +4,23 @@ using System.Windows.Shapes;
 
 namespace SnakeGame
 {
-    class Snake
-    {
-        public double x, y;
-        public Ellipse ellSnake = new Ellipse();
+     class Snake
+     {
+         public double x, y;
+         public Ellipse ellSnake = new Ellipse();
 
-        public Snake(double x,double y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-        public void SetSnakePosition()
-        {
-            ellSnake.Width = 12;
-            ellSnake.Height = 12;
-            ellSnake.Fill = Brushes.Red;
-            Canvas.SetLeft(ellSnake, x);
-            Canvas.SetTop(ellSnake, y);
-        }
+         public Snake(double x,double y)
+         {
+             this.x = x;
+             this.y = y;
+         }
+         public void SetSnakePosition()
+         {
+             ellSnake.Width = 12;
+             ellSnake.Height = 12;
+             ellSnake.Fill = Brushes.Red;
+             Canvas.SetLeft(ellSnake, x);
+             Canvas.SetTop(ellSnake, y);
+         }
     }
 }
